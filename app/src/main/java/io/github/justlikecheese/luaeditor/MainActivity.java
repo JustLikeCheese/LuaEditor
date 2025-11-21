3639e3c26d0345c36303f7960dc4de4739ecb1f7
JustLikeCheese <854675824@qq.com>
2025-11-21 15:38:56 +0800

    fix: layout loading failed

--- app/src/main/java/io/github/justlikecheese/luaeditor/MainActivity.java
+++ app/src/main/java/io/github/justlikecheese/luaeditor/MainActivity.java
@@ -2,11 +2,19 @@ package io.github.justlikecheese.luaeditor;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import com.androlua.LuaEditor;
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        LinearLayout layout = new LinearLayout(this);
+        layout.setFitsSystemWindows(true);
+        LuaEditor editor = new LuaEditor(this);
+        editor.setText("print('hello world')");
+        layout.addView(editor);
+        setContentView(layout);
     }
 }
